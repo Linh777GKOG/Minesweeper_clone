df93a9234f14f539276eb5167d20c016b3d2dc66
Linh Vu <linh777gkog@gmail.com>
2022-06-20 22:27:08 +0700

    sceneria

--- script.js
+++ script.js
@@ -31,3 +31,40 @@ board.forEach((row) => {
 });
 boardElement.style.setProperty('--size', BOARD_SIZE);
 minesLeftText.textContent = NUMBER_OF_MINES;
+
+function listMinesLeft() {
+  const markedTilesCount = board.reduce((count, row) => {
+    return (
+      count + row.filter((tile) => tile.status === TILE_STATUSES.MARKED).length
+    );
+  }, 0);
+
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
+}
+
+function checkGameEnd() {
+  const win = checkWin(board);
+  const lose = checkLose(board);
+
+  if (win || lose) {
+    boardElement.addEventListener('click', stopProp, { capture: true });
+    boardElement.addEventListener('contextmenu', stopProp, { capture: true });
+  }
+
+  if (win) {
+    messageText.textContent = 'You Win';
+  }
+  if (lose) {
+    messageText.textContent = 'You Lose';
+    board.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile.status === TILE_STATUSES.MARKED) markTile(tile);
+        if (tile.mine) revealTile(board, tile);
+      });
+    });
+  }
+}
+
+function stopProp(e) {
+  e.stopImmediatePropagation();
+}
